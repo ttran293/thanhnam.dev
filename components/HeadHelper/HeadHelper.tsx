a665f7475b2123577913529cf876d360d19ee173
Thanh Nam Tran <ttran19@umbc.edu>
2022-04-19 14:02:24 -0400

    separate head, create headhelper

---
+++ components/HeadHelper/HeadHelper.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import React from "react";
+
+
+export default function HeadHelper({title}:any){
+    return (
+      <Head>
+          <title>{title? title : 'Portfolio'} | Nam</title>
+          <meta name="description" content="Thanh Nam Tran Portfolio" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          ></meta>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+    );
+}
