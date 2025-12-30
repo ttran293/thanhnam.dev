58163a713659cb41e78c84851da0c076c9a83316
Thanh Nam Tran <ttran19@umbc.edu>
2025-12-30 10:33:12 -0500

    update

--- components/Footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styles from "./Footer.module.css"
-export default function Footer(){
-    return (
-        <footer className={styles.footer}>Copyright © Nam</footer>
-    );
-}
